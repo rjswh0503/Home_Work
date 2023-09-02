426248517d44d400a04d02fdc44c8e8c97b507d4
rjswh0503 <141482043+rjswh0503@users.noreply.github.com>
2023-09-02 10:16:15 +0900

    Home_Work

---
+++ src/com/kh/practice4/exam/Practice7.java
@@ -0,0 +1,16 @@
+package com.kh.practice4.exam;
+
+import java.util.Scanner;
+
+public class Practice7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 : ");
+        int num = sc.nextInt();
+        System.out.println("==== " + num + " 단 ====");
+
+        for(int i = 1; i < num)
+
+
+    }
+}
