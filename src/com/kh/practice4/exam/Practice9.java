05317e3214a8018942cea8ff984d87e5eab75767
rjswh0503 <141482043+rjswh0503@users.noreply.github.com>
2023-09-02 20:44:05 +0900

    Home_Work

---
+++ src/com/kh/practice4/exam/Practice9.java
@@ -0,0 +1,53 @@
+package com.kh.practice4.exam;
+
+import java.util.Scanner;
+
+
+/*
+
+
+사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.
+단,9를 초과하는 숫자가 들어오면 "9 이하의 숫자만 입력해주세요."를 출력하세요.
+
+
+ */
+
+
+public class Practice9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 : ");
+        int num = sc.nextInt();
+
+
+
+        while(true){
+            if(num > 1){
+                for(int i = num; i <=9; i++){
+                    System.out.println("==== " + i + "단 ====");
+                    for(int j =1; j <=9; j++){
+                        System.out.println(i + " * " + j + " = " + i * j);
+
+                    }
+                    System.out.println();
+
+
+
+
+                }
+                break;
+            }else if(num >= 10 ){
+
+                System.out.println("9 이하의 숫자만 입력해주세요.");
+
+
+            }
+
+        }
+
+
+
+    }
+
+
+}
