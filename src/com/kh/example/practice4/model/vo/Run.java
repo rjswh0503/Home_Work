46f694a32cd6f85c03c2f5aaf538b5de540494c9
rjswh0503 <141482043+rjswh0503@users.noreply.github.com>
2023-09-04 22:45:40 +0900

    객체 실습

---
+++ src/com/kh/example/practice4/model/vo/Run.java
@@ -0,0 +1,15 @@
+package com.kh.example.practice4.model.vo;
+
+public class Run {
+    public static void main(String[] args) {
+        Student sc = new Student(85,"3반","신재헌",177.6,"M");
+
+
+
+        sc.information();
+
+
+
+
+    }
+}
---
+++ src/com/kh/example/practice4/model/vo/Student.java
@@ -0,0 +1,29 @@
+package com.kh.example.practice4.model.vo;
+
+public class Student {
+    int grade;
+    String classroom;
+    String name;
+    double height;
+    String gender;
+
+    public Student(int grade, String classroom, String name, double height, String gender){
+        this.grade = grade;
+        this.classroom = classroom;
+        this.name = name;
+        this.height = height;
+        this.gender = gender;
+    }
+
+    public void information(){
+
+        System.out.println(" 학점 : " + grade +"점");
+        System.out.println(" 반 : " + classroom);
+        System.out.println(" 이름 : " + name);
+        System.out.println(" 키 : " + height);
+        System.out.println(" 성별 : " + gender);
+
+    }
+
+
+}
