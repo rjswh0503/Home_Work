a818eb4c85336024f0493189fedd6fc9e176d0a1
rjswh0503 <141482043+rjswh0503@users.noreply.github.com>
2023-09-09 00:39:30 +0900

    HomeWork/230907

--- src/JAVA_HomeWork/HomeWork230907/Practice4.java
+++ src/JAVA_HomeWork/HomeWork230907/Practice4.java
@@ -3,39 +3,28 @@ package JAVA_HomeWork.HomeWork230907;
 import java.util.Scanner;
 
 public class Practice4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("정수 5개를 입력하세요 .");
 
 
+            int[] numbers = new int[5];
+            for (int i = 0; i < 5; i++) {
+                System.out.print("정수 " + (i) + " : ");
+                numbers[i] = sc.nextInt();
 
-        System.out.print("첫 번째 숫자 : ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자 : ");
-        int num2 = sc.nextInt();
-        System.out.print("세 번째 숫자 : ");
-        int num3 = sc.nextInt();
-        System.out.print("네 번째 숫자 : ");
-        int num4 = sc.nextInt();
-        System.out.print("다 섯번째 숫자 : ");
-        int num5 = sc.nextInt();
+            }
 
 
-        int[] numbers = {num1,num2,num3,num4,num5};
+            int sum = 0;
+            for (int num : numbers) {
+                sum += num;
+            }
 
 
-
-        int sum = 0;
-
-        for(int number : numbers){
-                sum += number;
-
+            System.out.println("총합 : " + sum);
 
 
         }
-        System.out.println("총합 : " + sum);
-
-
-
-
     }
-}
+
