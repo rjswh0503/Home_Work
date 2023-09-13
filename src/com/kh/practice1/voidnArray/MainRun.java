2bcf94784bdda6c941187609765afccc2f1275f3
rjswh0503 <141482043+rjswh0503@users.noreply.github.com>
2023-09-13 22:53:04 +0900

    HomeWork/230913 할 수 있는 곳 까지..

---
+++ src/com/kh/practice1/voidnArray/MainRun.java
@@ -0,0 +1,21 @@
+package com.kh.practice1.voidnArray;
+
+public class MainRun {
+
+    public static void main(String[] args) {
+        voidNReturn practice = new voidNReturn();
+        //practice.practice1();
+        //practice.practice2();
+        //practice.practice3();
+        //practice.practice4();
+        //practice.practice5();
+        //practice.practice6();
+       // practice.isPrimeNumber()
+        //practice.calculate();
+        practice.countWords( "안녕하세요안녕하세요.");
+
+
+
+
+    }
+}
